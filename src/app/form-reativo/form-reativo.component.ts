6c2ef4f4f600fc751d71cb9e715921d964a4e0f0
Rafaela Silverio <rafaelapetelin@gmail.com>
2025-04-28 10:58:01 -0300

    Projeto base

--- src/app/form-reativo/form-reativo.component.ts
+++ src/app/form-reativo/form-reativo.component.ts
@@ -1,37 +1,12 @@
-import { Component, OnInit } from '@angular/core';
-import {
-  FormBuilder,
-  FormGroup,
-  Validators,
-  ReactiveFormsModule,
-} from '@angular/forms';
-import { CommonModule } from '@angular/common';
+import { Component} from '@angular/core';
 
 @Component({
   selector: 'app-form-reativo',
   standalone: true,
-  imports: [CommonModule, ReactiveFormsModule],
+  imports: [],
   templateUrl: './form-reativo.component.html',
   styleUrls: ['./form-reativo.component.css'],
 })
-export class FormReativoComponent implements OnInit {
-  formularioUsuario!: FormGroup;
+export class FormReativoComponent{
 
-  constructor(private fb: FormBuilder) {}
-
-  ngOnInit() {
-    this.formularioUsuario = this.fb.group({
-      nome: ['', [Validators.required, Validators.minLength(3)]],
-      email: ['', [Validators.required, Validators.email]],
-      tipoConta: ['', Validators.required],
-      dataNascimento: ['', Validators.required],
-      receberNewsletter: [false],
-    });
-  }
-
-  aoEnviar() {
-    alert(
-      `Formulário enviado (template-driven): ${this.formularioUsuario.value.nome} teve seu cadastro bem sucedido!`
-    );
-  }
 }
--- src/app/form-template/form-template.component.ts
+++ src/app/form-template/form-template.component.ts
@@ -1,26 +1,12 @@
 import { Component } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-form-template',
   standalone: true,
-  imports: [CommonModule, FormsModule],
+  imports: [],
   templateUrl: './form-template.component.html',
   styleUrls: ['./form-template.component.css'],
 })
 export class FormTemplateComponent {
-  usuario = {
-    nome: '',
-    email: '',
-    tipoConta: '',
-    dataNascimento: '',
-    receberNewsletter: false,
-  };
 
-  aoEnviar() {
-    alert(
-      `Formulário enviado (template-driven): ${this.usuario.nome} teve seu cadastro bem sucedido!`
-    );
-  }
 }
